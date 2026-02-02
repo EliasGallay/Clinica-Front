86e1ef221effdcd383cf4ab232de58431eb519c3
20379016856 <damian9504@hotmail.com>
2026-02-02 14:08:53 -0300

    feature: agregado de estructura MUI, React hook form, Joi, y correccion login form

---
+++ src/components/formInputs/FormTextInput.tsx
@@ -0,0 +1,56 @@
+import { FormHelperText, InputLabel, TextFieldProps, Typography } from '@mui/material';
+import { TextInput } from '../ui/TextInput';
+import { HookFormProps } from '@/types/forms/HookFormProps';
+import { Controller, FieldValues } from 'react-hook-form';
+
+export const FormTextInput = <TFieldValues extends FieldValues = FieldValues>(
+  props: TextFieldProps & HookFormProps<TFieldValues>
+) => {
+  const {
+    variant = 'outlined',
+    type = 'text',
+    label,
+    helperText,
+    error = false,
+    control,
+    name,
+    disabled = false,
+    ...rest
+  } = props;
+  console.log('🚀 ~ onSubmit ~ helperText:', helperText);
+
+  return (
+    <>
+      <div>
+        {label && (
+          <InputLabel id={`${rest.id}-label`}>
+            <Typography
+              variant="body1"
+              sx={{
+                fontWeight: 'bold',
+                color: 'black',
+              }}
+            >
+              {label}
+            </Typography>
+          </InputLabel>
+        )}
+      </div>
+      <Controller
+        control={control}
+        name={(name || '') as never}
+        disabled={disabled}
+        render={({ field }) => {
+          return (
+            <TextInput {...field} variant={variant} type={type} error={error} fullWidth {...rest} />
+          );
+        }}
+      />
+      {helperText && (
+        <FormHelperText error={error} sx={{ mt: 0 }}>
+          <Typography variant="body5">{helperText}</Typography>
+        </FormHelperText>
+      )}
+    </>
+  );
+};
